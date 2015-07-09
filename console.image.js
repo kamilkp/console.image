57ecb88ffe8d21605c044fa1f6842533dd70fc9f
Kamil Pękala <kamilkp@gmail.com>
2015-07-09 10:14:03 +0200

    commented out console.errors

--- console.image.js
+++ console.image.js
@@ -114,8 +114,8 @@
 	 * @return {null}
 	 */
 	console.meme = function(upper, lower, image, width, height) {
-		if(!upper) console.error("Yo, you forgot the text for the upper part of the meme. The bit at the top. Yeah, that bit.");
-		if(!lower) console.error("You forgot the text for the bottom of the meme, stupid.");
+		// if(!upper) console.error("Yo, you forgot the text for the upper part of the meme. The bit at the top. Yeah, that bit.");
+		// if(!lower) console.error("You forgot the text for the bottom of the meme, stupid.");
 		if(!image) console.error("Dude, you forgot the meme type or url for the background image (CORS enabled, *hint* imgur *hint*). To see a list of supported memes, hit `console.meme()`");
 		if(!upper && !lower && !image) return console.log("> " + Object.keys(memes).join("\n> "));
 
